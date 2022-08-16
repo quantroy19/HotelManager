3e9156944788ace7adb2409654affb12b00f4042
quantroy19 <doquan285lg@gmail.com>
2022-08-16 05:53:52 +0000

    fix-booking

--- resources/views/admin/booking/edit.blade.php
+++ resources/views/admin/booking/edit.blade.php
@@ -22,72 +22,139 @@
                         </div>
                     @endif
                 </div>
-                <div class="card-body col-6">
-                    <form method="POST" action="{{ route('admin.booking.update', $id) }}">
-                        @csrf
-                        @method('PUT')
-                        <div class="form-group">
-                            <label>Room</label>
-                            <select class="custom-select form-control" name="room_id" id="room_id1">
-                                @foreach ($rooms as $room)
-                                    <option {{ $room->id == $item->room_id ? 'selected' : '' }}
-                                        room_id="{{ $room->id }}" value="{{ $room->id }}">{{ $room->name }}
-                                    </option>
-                                @endforeach
-                            </select>
-                        </div>
-                        @error('room_id')
-                            <div class="alert alert-danger error">{{ $message }}</div>
-                        @enderror
-                        <div class="form-group">
-                            <label>Arrival date</label>
-                            <input class="form-control" name="arrival_date" id="from-picker" type="text"
-                                autocomplete="off" placeholder="Arrival date " value="{{ $item->arrival_date }}">
-                        </div>
-                        @error('arrival_date ')
-                            <div class="alert alert-danger error">{{ $message }}</div>
-                        @enderror
-                        <div class="form-group">
-                            <label>Departure date</label>
-                            <input class="form-control" name="departure_date" id="to-picker"type="text"
-                                autocomplete="off" placeholder="departure_date" value="{{ $item->departure_date }}">
-                        </div>
-                        @error('departure_date')
-                            <div class="alert alert-danger error">{{ $message }}</div>
-                        @enderror
-                        <div class="form-group">
-                            <label>Name</label>
-                            <input type="text" class="form-control" name="name" placeholder="name"
-                                value="{{ $item->name }}">
-                        </div>
-                        @error('name')
-                            <div class="alert alert-danger error">{{ $message }}</div>
-                        @enderror
-                        <div class="form-group">
-                            <label>Phone</label>
-                            <input type="text" class="form-control" name="phone" placeholder="phone"
-                                value="{{ $item->phone }}">
-                        </div>
-                        @error('phone')
-                            <div class="alert alert-danger error">{{ $message }}</div>
-                        @enderror
-                        <div class="form-group">
-                            <label>Email</label>
-                            <input type="text" class="form-control" name="email" placeholder="email"
-                                value="{{ $item->email }}">
-                        </div>
-                        @error('email')
-                            <div class="alert alert-danger error">{{ $message }}</div>
-                        @enderror
-                        <div class="form-group">
-                            <label>Infomation</label>
-                            <textarea style="height: 100px" class="form-control" name="infomation" id="" cols="50" rows="30">{{ $item->infomation }}</textarea>
-                        </div>
-                        @error('infomation')
-                            <div class="alert alert-danger error">{{ $message }}</div>
-                        @enderror
-                        <button type="submit" class="btn btn-info btn-fill">Submit</button>
-                    </form>
+                <div class="row p-3">
+                    <div class="col-6">
+                        <form method="POST" action="{{ route('admin.booking.update', $id) }}">
+                            @csrf
+                            @method('PUT')
+                            <div class="form-group">
+                                <label>Room</label>
+                                <select class="custom-select form-control" name="room_id" id="room_id1">
+                                    @foreach ($rooms as $room)
+                                        <option {{ $room->id == $item->room_id ? 'selected' : '' }}
+                                            room_id="{{ $room->id }}" value="{{ $room->id }}">{{ $room->name }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            @error('room_id')
+                                <div class="alert alert-danger error">{{ $message }}</div>
+                            @enderror
+                            <div class="form-group">
+                                <label>Arrival date</label>
+                                <input class="form-control" name="arrival_date" id="from-picker" type="text"
+                                    autocomplete="off" placeholder="Arrival date " value="{{ $item->arrival_date }}">
+                            </div>
+                            @error('arrival_date ')
+                                <div class="alert alert-danger error">{{ $message }}</div>
+                            @enderror
+                            <div class="form-group">
+                                <label>Departure date</label>
+                                <input class="form-control" name="departure_date" id="to-picker"type="text"
+                                    autocomplete="off" placeholder="departure_date" value="{{ $item->departure_date }}">
+                            </div>
+                            @error('departure_date')
+                                <div class="alert alert-danger error">{{ $message }}</div>
+                            @enderror
+                            <div class="form-group">
+                                <label>Name</label>
+                                <input type="text" class="form-control" name="name" placeholder="name"
+                                    value="{{ $item->name }}">
+                            </div>
+                            @error('name')
+                                <div class="alert alert-danger error">{{ $message }}</div>
+                            @enderror
+                            <div class="form-group">
+                                <label>Phone</label>
+                                <input type="text" class="form-control" name="phone" placeholder="phone"
+                                    value="{{ $item->phone }}">
+                            </div>
+                            @error('phone')
+                                <div class="alert alert-danger error">{{ $message }}</div>
+                            @enderror
+                            <div class="form-group">
+                                <label>Email</label>
+                                <input type="text" class="form-control" name="email" placeholder="email"
+                                    value="{{ $item->email }}">
+                            </div>
+                            @error('email')
+                                <div class="alert alert-danger error">{{ $message }}</div>
+                            @enderror
+                            <div class="form-group">
+                                <label>Infomation</label>
+                                <textarea style="height: 133px" class="form-control" name="infomation" id="" cols="50" rows="30">{{ $item->infomation }}</textarea>
+                            </div>
+                            @error('infomation')
+                                <div class="alert alert-danger error">{{ $message }}</div>
+                            @enderror
+                            <button type="submit" class="btn btn-info btn-fill">Submit</button>
+                        </form>
+                    </div>
+                    <div class="col-6">
+                        @if (Session::has('success'))
+                            <div class="alert alert-success alert-dismissible" role="alert">
+                                <strong>{{ Session::get('success') }}</strong>
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    <span class="sr-only">Close</span>
+                                </button>
+                            </div>
+                        @endif
+                        <form method="POST" action="{{ route('admin.book.sendMailPay') }}">
+                            @csrf
+                            <h2>Thanh toán</h2>
+                            <input type="hidden" name="user_name" value="{{ $item->user->name }}">
+                            <div class="form-group">
+                                <label>Arrival date</label>
+                                <input class="form-control" name="arrival_date" id="from-picker" type="text"
+                                    autocomplete="off" placeholder="Arrival date " value="{{ $item->arrival_date }}">
+                            </div>
+                            @error('arrival_date ')
+                                <div class="alert alert-danger error">{{ $message }}</div>
+                            @enderror
+                            <div class="form-group">
+                                <label>Departure date</label>
+                                <input class="form-control" name="departure_date" id="to-picker"type="text"
+                                    autocomplete="off" placeholder="departure_date" value="{{ $item->departure_date }}">
+                            </div>
+                            @error('departure_date')
+                                <div class="alert alert-danger error">{{ $message }}</div>
+                            @enderror
+                            <div class="form-group">
+                                <label>Name</label>
+                                <input type="text" class="form-control" name="name" placeholder="name"
+                                    value="{{ $item->room->name }}">
+                            </div>
+                            @error('name')
+                                <div class="alert alert-danger error">{{ $message }}</div>
+                            @enderror
+                            <div class="form-group">
+                                <label>Price/ day</label>
+                                <input type="text" class="form-control" name="price" placeholder="phone"
+                                    value="{{ $item->room->price }}">
+                            </div>
+                            @error('phone')
+                                <div class="alert alert-danger error">{{ $message }}</div>
+                            @enderror
+                            <div class="form-group">
+                                <label>Email</label>
+                                <input type="text" class="form-control" name="email" placeholder="email"
+                                    value="{{ $item->email }}">
+                            </div>
+
+                            <div class="form-group">
+                                <label>Total day booked</label>
+                                <input type="text" class="form-control" name="total_day_booked" placeholder="email"
+                                    value="{{ $totalDayBooked }}">
+                            </div>
+                            <div class="form-group">
+                                <label>Total price</label>
+                                <input type="text" class="form-control" name="total_price" placeholder="email"
+                                    value="{{ $totalPrice }}">
+                            </div>
+                            <button type="submit" class="btn btn-success btn-fill">Send mail</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ resources/views/admin/mail/pay-booking.blade.php
@@ -0,0 +1,48 @@
+{{-- <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ __('Beyond hotel') }}</title>
+</head>
+
+<body style="display: flex;justify-content: center;">
+    <div style="width: 70%;padding: 10px; margin: 0 auto">
+        <div class="img">
+            <img src="http://news365htmllatest.bdtask.com/Demo/DemoNews365/images/logo.png" alt="logo"
+                width="50%">
+        </div>
+        <h1 style="color: rgb(5, 215, 215)">{{ __('Pay Booking') }}</h1>
+        <p>{{ __('Hello') }}, <b style="color: rgb(240, 84, 84)">{{ $content['user_name'] }}</b></p>
+
+        <table border="1" style="border-collapse: collapse" cellpadding="10px">
+            <thead>
+                <tr>
+                    <th style="background-color: aquamarine">{{ __('ARRIVAL DATE') }}</th>
+                    <th style="background-color: aquamarine">{{ __('DEPARTURE DATE') }}</th>
+                    <th style="background-color: aquamarine">{{ __('NAME ROOM') }}</th>
+                    <th style="background-color: aquamarine">{{ __('PRICE/ DAY') }}</th>
+                    <th style="background-color: aquamarine">{{ __('TOTAL DAY BOOKED') }}</th>
+                    <th style="background-color: aquamarine">{{ __('TOTAL PRICE') }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                    </td>
+                    <td>{{ $post->user->name }}</td>
+                    <td>{{ $post->status }}</td>
+                    <td>{{ $post->created_at }}</td>
+                </tr>
+
+            </tbody>
+        </table>
+
+
+        <p>{{ __('From :name', ['name' => config('app.name')]) }}</p>
+    </div>
+</body>
+
+</html> --}}
